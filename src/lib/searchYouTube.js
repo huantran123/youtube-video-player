3d6864a7d015032c26f16965d67b48cd28e712b2
Huan Tran <huanductran98@gmail.com>
2022-07-12 21:11:19 -0700

    Processing REST API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -5,7 +5,54 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
 });
 
 var searchYouTube = (query, callback) => {
-  // TODO
+  // create: function(message, successCB, errorCB = null) {
+  //   $.ajax({
+  //     url: searchYouTube.server,
+  //     type: 'POST',
+  //     data: JSON.stringify(message),
+  //     contentType: 'application/json',
+  //     success: successCB,
+  //     error: errorCB || function(error) {
+  //       console.error('chatterbox: Failed to post message', error);
+  //     }
+  //   });
+  // },
+  $.ajax({
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos/',
+    type: 'GET',
+    data: { order: '-createdAt' },
+    contentType: 'application/json',
+    success: function(data) {
+      console.log(data);
+      // data.forEach(item => {
+      //   if(item.title.subString(query)){
+      //     showit!
+      //   };
+      // });
+    },
+    error: function(error) {
+      console.error('FAIL: ', error);
+    }
+  });
+  // var getData = function(sucessCB, errorCB) {
+  //   $.ajax({
+  //     url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos/',
+  //     type: 'GET',
+  //     data: { order: '-createdAt' },
+  //     contentType: 'application/json',
+  //     success: function(data){
+  //       console.log(data);
+  //     },
+  //     error: errorCB || function(error) {
+  //       console.error('FAIL: ', error);
+  //     }
+  //   });
+  // };
+
+  // getData((data) => {
+  //   console.log(data);
+  // });
+
 };
 
 export default searchYouTube;
